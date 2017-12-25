748795201cd24301aa56f9090f5c2d49ca020f74
pawel <1464353296@qq.com>
2017-12-25 09:47:53 +0800

    first commit

---
+++ data.js
@@ -0,0 +1,14 @@
+var css = [
+	{
+		type: 'display',
+		css2: ['none', 'inline', 'block', 'list-item', 'inline-block', 'table', 'inline-table', 'table-caption', 'table-cell', 'table-row', 'table-row-group', 'table-column', 'table-column-group', 'table-footer-group', 'table-header-group'],
+		css3: ['run-in', 'box', 'inline-box', 'flexbox', 'inline-flexbox', 'flex', 'inline-flex', 'grid', 'inline-grid'],
+		prefix: ['-webkit-box', '-webkit-inline-box']
+	},
+	{
+		type: 'position',
+		css2: ['static', 'relative', 'absolute', 'fixed'],
+		css3: ['center', 'page', 'sticky'],
+		prefix: ['-webkit-sticky']
+	}
+];
